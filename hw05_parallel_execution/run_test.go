482951b0aa31a1e8ba9d05b0e930358c60e93513
Oleg Togushakov <oleg.togushakov@eurostudio.net>
2022-06-27 11:12:43 +0700

    HW05 is completed!

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -67,4 +67,113 @@ func TestRun(t *testing.T) {
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
+
+	t.Run("max errors count equals 0 or less", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+
+			tasks = append(tasks, func() error {
+				time.Sleep(taskSleep)
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+
+		workersCount := 5
+		maxErrorsCount := -1
+
+		err := Run(tasks, workersCount, maxErrorsCount)
+		require.NoError(t, err)
+
+		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
+	})
+
+	t.Run("no tasks", func(t *testing.T) {
+		tasksCount := 0
+		tasks := make([]Task, 0, tasksCount)
+
+		workersCount := 5
+		maxErrorsCount := 1
+
+		err := Run(tasks, workersCount, maxErrorsCount)
+		require.NoError(t, err)
+	})
 }
+
+// TODO: Прошу помочь с написанием теста.
+// В соответствии с комментарием от Олега Венгера:
+// "привет, всё верно. необходимо ограничить сверху время ожидания завершения тасок.
+// Но тут скорее нужно подумать, как можно запустить n тасок чтобы они все были заблокированы и чего-то ждали.
+// А потом сразу всем сказать закончили.
+// Тогда в целом при запуске eventually можно прикинуть, что для завершения всех тасок потребуется сколько-то времени"
+
+// Я, с помощью sync.Cond, смог запустить все таски одновременно и
+// заставить их ждать сигнала. Но, как посчитать время их выполнения для
+// аргумента waitFor у require.Eventually, я не могу понять.
+// Подскажите, пожалуйста, в верном направлении я двигаюсь? Мне жутко интересно, как это можно сделать
+
+// Тест-кейс представлен ниже
+
+// t.Run("tasks without errors with eventually", func(t *testing.T) {
+//	tasksCount := 50
+//	tasks := make([]Task, 0, tasksCount)
+//
+//	var runTasksCount int32
+//
+//	wg := sync.WaitGroup{}
+//	once := sync.Once{}
+//	cond := sync.NewCond(&sync.Mutex{})
+//	workersCount := tasksCount
+//	maxErrorsCount := 1
+//
+//	var start time.Time
+//	var elapsed time.Duration
+//
+//	for i := 0; i < tasksCount; i++ {
+//		tasks = append(tasks, func() error {
+//			once.Do(func() {
+//				start = time.Now()
+//			})
+//
+//			atomic.AddInt32(&runTasksCount, 1)
+//			cond.L.Lock()
+//			cond.Wait()
+//			cond.L.Unlock()
+//			return nil
+//		})
+//	}
+//
+//	go func(wg *sync.WaitGroup) {
+//		wg.Add(1)
+//		defer wg.Done()
+//		tasksCountInt32 := int32(tasksCount)
+//		for {
+//			if atomic.LoadInt32(&runTasksCount) == tasksCountInt32 {
+//				cond.Broadcast()
+//				elapsed = time.Since(start)
+//				break
+//			}
+//		}
+//	}(&wg)
+//
+//	condition := func() bool {
+//		err := Run(tasks, workersCount, maxErrorsCount)
+//		return err == nil
+//	}
+//
+//	//err := Run(tasks, workersCount, maxErrorsCount)
+//	//require.NoError(t, err)
+//
+//	require.Eventually(t, condition, time.Second*10, 1*time.Millisecond, "tasks were run sequentially?")
+//	//require.Eventually(t, func() bool {
+//	//	return true
+//	//}, elapsed, 1*time.Millisecond, "tasks were run sequentially?")
+//	require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
+//	fmt.Println(elapsed)
+//	wg.Wait()
+// })
