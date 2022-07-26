891fcbf7150e6e245789a0e2af00cac4f8edb27f
Oleg Togushakov <togushakov@gmail.com>
2022-07-26 13:11:09 +0700

    HW08 is completed

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,46 @@
 package main
 
+import (
+	"errors"
+	"io"
+	"os"
+	"os/exec"
+)
+
+const ErrorExitCode = 1
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
-func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+func RunCmd(cmd []string, env Environment, stdin io.Reader, stdout, stderr io.Writer) (returnCode int) {
+	for k, v := range env {
+		var err error
+		if v.NeedRemove {
+			err = os.Unsetenv(k)
+		} else {
+			err = os.Setenv(k, v.Value)
+		}
+
+		if err != nil {
+			returnCode = ErrorExitCode
+			return
+		}
+	}
+
+	c := cmd[0]
+	params := cmd[1:]
+	command := exec.Command(c, params...)
+
+	command.Stdin = stdin
+	command.Stdout = stdout
+	command.Stderr = stderr
+	command.Env = os.Environ()
+
+	if err := command.Run(); err != nil {
+		var exitErr *exec.ExitError
+		if errors.As(err, &exitErr) {
+			return exitErr.ExitCode()
+		}
+		return ErrorExitCode
+	}
+
+	return command.ProcessState.ExitCode()
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 2 {
+		log.Fatal("Invalid arguments count. Must be 2 or greater")
+	}
+
+	env, err := ReadDir(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	os.Exit(RunCmd(os.Args[2:], env, os.Stdin, os.Stdout, os.Stderr))
 }
