8e21f0c51d577afcd461ba3a3210a613bc0a6ee2
Oleg Togushakov <togushakov@gmail.com>
2022-08-09 21:13:59 +0700

    HW10 is completed

---
+++ hw10_program_optimization/stats_bench_test.go
@@ -0,0 +1,40 @@
+package hw10programoptimization
+
+import (
+	"archive/zip"
+	"flag"
+	"testing"
+)
+
+var isOld = flag.Bool("old", false, "bench non optiomized version of GetDomainStat (default is false)")
+
+func BenchmarkGetDomainStat(b *testing.B) {
+	domain := "biz"
+	becnhingFuncName := "GetDomainStat"
+	if *isOld {
+		becnhingFuncName = "GetDomainStatOld"
+	}
+
+	b.Logf("benching %s", becnhingFuncName)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		reader, err := zip.OpenReader("./testdata/users.dat.zip")
+		if err != nil {
+			b.Fatal()
+		}
+
+		file, err := reader.File[0].Open()
+		if err != nil {
+			b.Fatal()
+		}
+
+		if *isOld {
+			_, _ = GetDomainStatOld(file, domain)
+			continue
+		}
+
+		_, _ = GetDomainStat(file, domain)
+		_ = reader.Close()
+	}
+}
---
+++ hw10_program_optimization/stats_old.go
@@ -0,0 +1,55 @@
+package hw10programoptimization
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"regexp"
+	"strings"
+)
+
+func GetDomainStatOld(r io.Reader, domain string) (DomainStat, error) {
+	u, err := getUsers(r)
+	if err != nil {
+		return nil, fmt.Errorf("get users error: %w", err)
+	}
+	return countDomains(u, domain)
+}
+
+type usersOld [100_000]User
+
+func getUsers(r io.Reader) (result usersOld, err error) {
+	content, err := ioutil.ReadAll(r)
+	if err != nil {
+		return
+	}
+
+	lines := strings.Split(string(content), "\n")
+	for i, line := range lines {
+		var user User
+		if err = json.Unmarshal([]byte(line), &user); err != nil {
+			return
+		}
+		result[i] = user
+	}
+	return
+}
+
+func countDomains(u usersOld, domain string) (DomainStat, error) {
+	result := make(DomainStat)
+
+	for _, user := range u {
+		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
+		if err != nil {
+			return nil, err
+		}
+
+		if matched {
+			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
+			num++
+			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
+		}
+	}
+	return result, nil
+}
