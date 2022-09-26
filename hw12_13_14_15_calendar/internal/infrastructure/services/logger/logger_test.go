faf8112438a19f66d65a0f0c40c5c974e2f1eae3
otogushakov <otogushakov@ptsecurity.com>
2022-09-26 15:08:01 +0700

    HW12 add project structure
    Add logger tests

--- hw12_13_14_15_calendar/internal/infrastructure/services/logger/logger_test.go
+++ hw12_13_14_15_calendar/internal/infrastructure/services/logger/logger_test.go
@@ -1,7 +1,42 @@
 package logger
 
-import "testing"
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"log"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestLogger(t *testing.T) {
-	// TODO:
+	var buf bytes.Buffer
+
+	logger := NewLogger("WarN", true, &buf)
+	require.NotNil(t, logger)
+
+	t.Run("is different levels are printed", func(t *testing.T) {
+		reader := bufio.NewReader(&buf)
+
+		logger.Info("Information")
+		output, err := reader.ReadBytes('\n')
+		if err != nil && err != io.EOF {
+			log.Fatal(err)
+		}
+
+		message := string(output)
+		require.Empty(t, message)
+
+		buf.Reset()
+
+		logger.Warn("Warning")
+		output, err = reader.ReadBytes('\n')
+		if err != nil && err != io.EOF {
+			log.Fatal(err)
+		}
+
+		message = string(output)
+		require.Contains(t, message, "Warning")
+	})
 }
